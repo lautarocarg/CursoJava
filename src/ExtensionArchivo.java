2bdfc1edf334c82dd32d6acc61a6d074d6b802cb
Lautaro Cargnelutti <cargneluttilautaro@gmail.com>
2022-09-02 19:14:53 -0300

    Trabajando sobre la extension de los archivos con String

--- src/ExtensionArchivo.java
+++ src/ExtensionArchivo.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class ExtensionArchivo {
+public class ExtensionArchivo {
+    public static void main(String[] args) {
+        String archivo = "alguna_imagen.pdf";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("archivo= " + archivo.substring( i + 1 ));
+
+    }
 }
