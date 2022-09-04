78e277014ec412254fa8440a7a906e4243efa0e6
Lautaro Cargnelutti <cargneluttilautaro@gmail.com>
2022-09-04 16:39:07 -0300

    Introduccion flujos de control

---
+++ src/SentenciaSCNumDias.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class SentenciaSCNumDias {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el mes: ");
+        int mes = scanner.nextInt();
+        int numeroDias = 0;
+        System.out.print("Ingrese el año: ");
+        int anio = scanner.nextInt();
+
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numeroDias = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numeroDias = 30;
+                break;
+            case 2:
+                if(anio % 400 == 0 || (anio % 4 == 0 & !(anio % 100 == 0)) ){
+                    numeroDias = 29;
+                }
+                else {
+                    numeroDias = 28;
+                }
+                break;
+            default:
+                numeroDias = 0;
+
+        }
+        /*if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+            numeroDias = 31;
+        }
+        else if (mes == 4 || mes == 6 || mes == 9 || mes == 11){
+            numeroDias = 30;
+        }
+        else if (mes == 2){
+            if(anio % 400 == 0 || (anio % 4 == 0 & !(anio % 100 == 0)) ){
+                numeroDias = 29;
+            }
+            else {
+                numeroDias = 28;
+            }
+        }*/
+
+        System.out.println("numeroDias = " + numeroDias);
+    }
+}
